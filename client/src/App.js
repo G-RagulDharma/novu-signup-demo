09d56bc46f3817be9fac3d1789aa04f6bc128c53
G-RagulDharma <ragulsoorya27@gmail.com>
2026-04-15 21:55:25 +0530

    [Feature]: Update Api Url

--- client/src/App.js
+++ client/src/App.js
@@ -20,7 +20,7 @@ function App() {
     setMsg("");
 
     try {
-      const res = await fetch("http://localhost:5000/signup", {
+      const res = await fetch("https://novu-signup-demo.onrender.com/signup", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
